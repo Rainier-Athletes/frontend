f8c7c1212816b2a833bfc3587c4fb98aa5614db7
Noah Israel <noahmvf@gmail.com>
2018-08-16 15:40:02 -0700

    trying to get procfile to work

---
+++ app/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const build = `${__dirname}/app/build`;
+console.log('this is ' + __dirname);
+
+app.use(express.static(build));
+
+app.get('*', (request, response) => {
+  response.sendFile(`${build}/index.html`);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('__SERVER_UP__', process.env.PORT); // eslint-disable-line
+});

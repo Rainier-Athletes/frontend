897a54208612c70563ac2fda6291c969eab3d8ff
Tracy Williams <selpilot@gmail.com>
2018-09-05 14:59:56 -0700

    removed .scoring object from point-tracker. moved its contents into subject object. needless nesting...

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -5,4 +5,4 @@ export const ADMIN_ROUTE = '/admin';
 export const MENTOR_ROUTE = '/mentor';
 export const DASHBOARD_ROUTE = '/dashboard';
 export const POINTS_TRACKER_ROUTE = '/pointstracker';
-export const SYNOPSIS_REPORT = '/synopsis'
+export const SYNOPSIS_REPORT = '/synopsis';

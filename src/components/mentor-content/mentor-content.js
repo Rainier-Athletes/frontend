d908ab8ce46b37a972035a88330f7f6eab1fa120
Karen <karenbtlai@gmail.com>
2018-09-30 22:52:55 -0400

    student profile styling layout

--- src/components/mentor-content/mentor-content.js
+++ src/components/mentor-content/mentor-content.js
@@ -5,6 +5,57 @@ import './_mentor-content.scss';
 
 class MentorContent extends React.Component {
   render() {
+    const student = this.props.content;
+    console.log(student);
+    const studentProfile = (
+      <div className="student-profile container">
+        <div className="profile-primary row">
+          <div>
+            <span className="info name">{ student.firstName } { student.lastName } </span>
+            <span className="icon">{ student.studentData ? student.studentData.dateOfBirth : null } </span>
+            <span className="icon">{ student.studentData ? student.studentData.school[0] : null } </span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <span className="title">Contact</span>
+            <span className="icon">{ student.phone } </span>
+            <span className="icon">{ student.primaryEmail } </span>
+          </div>
+          <div className="col-md-6">
+            <span className="title">Synergy Account</span>
+            <span className="icon">username</span>
+            <span className="icon">password</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="profile-link">
+            <a className="btn-link-1" href={ student.studentData ? student.studentData.googleDocsUrl : null } >Google Docs</a>
+            <a className="btn-link-1" href={ student.studentData ? student.studentData.googleCalendarUrl : null }>Google Calendar</a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <span className="title">Sport Info</span>
+            <span className="label">Sport</span>
+            <span className="label">Team</span>
+            <span className="label">Coach</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <span className="title">Family Info</span>
+              <span className="label">Name</span>
+              <span className="label">Rel.</span>
+              <span className="label">Email</span>
+              <span className="label">Cell</span>
+              <span className="label">Address</span>
+              <span className="label">Others</span>
+          </div>
+        </div>
+      </div>
+    );
+
     return (
       <React.Fragment>
       <div role="main" className="col-md-8 panel">
@@ -13,16 +64,13 @@ class MentorContent extends React.Component {
             Student Profile
           </a>
           {
-            this.props.content ? <a className="mentor-btn" onClick={ this.props.buttonClick }>
+            student ? <a className="mentor-btn" onClick={ this.props.buttonClick }>
                 Point Tracker
             </a> : null
           }
-
-          <div style={{ width: 'auto', overflow: 'auto', padding: '20px' }}>
           {
-            JSON.stringify(this.props.content)
+            student.studentData ? studentProfile : null
           }
-          </div>
         </div>
       </div>
       {

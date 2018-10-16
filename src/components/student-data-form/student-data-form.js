99bb94e7d299db383742bb1fe890b92455172597
Tracy Williams <selpilot@gmail.com>
2018-10-15 21:00:21 -0700

    reapplying fix

--- src/components/student-data-form/student-data-form.js
+++ src/components/student-data-form/student-data-form.js
@@ -347,7 +347,7 @@ class StudentDataForm extends React.Component {
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title title">Student Profile</h5>
-              <button type="button" className="close" onClick={ this.props.onCancel } data-dismiss="modal" aria-label="Close">
+              <button type="button" className="close" onClick={this.props.onCancel} data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>

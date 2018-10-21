6a10271ef3fe9281767bd8cf564deeb20e7c4bba
Karen <karenbtlai@gmail.com>
2018-10-21 19:34:21 -0400

    top toolbar styling

--- src/components/admin-table/admin-table.js
+++ src/components/admin-table/admin-table.js
@@ -581,8 +581,8 @@ class AdminTable extends React.Component {
             ? <StudentDataModal onClose={this.toggleSdModal()} onCancel={this.toggleSdModal(true)} studentId={this.state.studentSelected}></StudentDataModal> : null }
           <Prompt when={this.state.gridModified} message="Unsaved changes. Are you sure you want to leave?" />
           <div className="top-toolbar">
-            <button className="importBtn" onClick={ this.toggleAdminExtractModal }>Export CSV</button>
-            <button className="exportBtn">Import CSV</button>
+            <button className="toolbar-btn" onClick={ this.toggleAdminExtractModal }>Export CSV</button>
+            <button className="toolbar-btn">Import CSV</button>
             <button className={`updateBtn ${this.state.gridModified ? 'saveAlert' : ''}`} onClick={ this.toggleSaveTableModal }>Save Table</button>
           </div>
           <ReactDataGrid

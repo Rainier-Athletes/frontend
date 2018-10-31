8e95bff66759c6e665da4d8a87aefa4e691f6893
Liz Petersen <lizpetersen928@gmail.com>
2018-10-31 16:24:52 -0700

    adds help link to navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -103,6 +103,9 @@ class Navbar extends React.Component {
                 <a className="dropdown-item" onClick={ this.props.doLogout }>Logout</a>
               </div>
             </li>
+            <li className="nav-item">
+              <a className="nav-link help" href="https://docs.google.com/presentation/d/e/2PACX-1vTRSMVBEvObOl1sCKPmXMChP8A4eZScVmrRrzS6mDw0Imi5LkbFd1sSgqDS-QEPcBD-gvBFwmanrPIC/pub?start=true&loop=false&delayms=3000"alt="Help Documentation" target="_blank" rel="noopener noreferrer">Help</a>
+            </li>
           </ul>
         </div>
       </React.Fragment>
